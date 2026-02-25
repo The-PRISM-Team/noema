a6058094b1e0aa239bad71d48c0b30c225e8cf0b
Sophia Blair <sophb.ccjt@proton.me>
2026-02-25 07:53:03 -0300

    oh i think i figured it out

--- assets/scripts/ui.js
+++ assets/scripts/ui.js
@@ -438,12 +438,13 @@ function notify(title, text, icon) {
             queuedNotifs.push({id: notifDiv.id, when: Date.now()});
             const checkInterval = setInterval(() => {
                 if (Object.keys(notifElements).length < 6 && started && document.hasFocus()) {
-                    console.log(queuedNotifs)
+                    console.log('before:', queuedNotifs)
                     if (queuedNotifs[0]?.id === notifDiv.id) {
-                        queuedNotifs.shift();
                         if (Date.now() - queuedNotifs[0]?.when <= 60e3) notifHandler();
+                        queuedNotifs.shift();
                         clearInterval(checkInterval);
                     }
+                    console.log('after:', queuedNotifs)
                 }
             }, 100);
         } else {
