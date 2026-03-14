6c11df045690e38aa25981b711fdc34fd5446411
Sophia Blair <sophb.ccjt@proton.me>
2026-03-14 02:26:57 -0300

    add fallback fallbackLocale

--- src/scripts/lang.js
+++ src/scripts/lang.js
@@ -1,6 +1,6 @@
 const locales = Object.fromEntries(globalThis.localesArray.map(v => [v.lang, v]));
-const getLocaleStr = (key, fallbackLocale) => locale[key] ?? locales[fallbackLocale][key];
-const getLocaleTempStr = (key, fallbackLocale, vars = {}) => {
+const getLocaleStr = (key, fallbackLocale = 'enUS') => locale[key] ?? locales[fallbackLocale][key];
+const getLocaleTempStr = (key, fallbackLocale = 'enUS', vars = {}) => {
 	let str = getLocaleStr(key, fallbackLocale);
 	Object.entries(vars).forEach(([name, value]) => {
 		str = str.replaceAll(`{${name}}`, value);
