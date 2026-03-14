23907d0a534de0d8bfd13c963adb38dea3c1496b
Sophia Blair <sophb.ccjt@proton.me>
2026-03-14 02:47:24 -0300

    oops

--- src/scripts/lang.js
+++ src/scripts/lang.js
@@ -1,5 +1,5 @@
 const locales = Object.fromEntries(globalThis.localesArray.map(v => [v.lang, v]));
-const getLocaleStr = (key, fallbackLocale = 'enUS') => locale[key] ?? locales[fallbackLocale][key] ?? `Missing locale entry (${key}@${locale}|${fallbackLocale})! This is a bug, please report it!`;
+const getLocaleStr = (key, fallbackLocale = 'enUS') => locale[key] ?? locales[fallbackLocale]?.[key] ?? `Missing locale entry (${key}@${locale}|${fallbackLocale})! This is a bug, please report it!`;
 const getLocaleTempStr = (key, fallbackLocale = 'enUS', vars = {}) => {
 	let str = getLocaleStr(key, fallbackLocale);
 	Object.entries(vars).forEach(([name, value]) => {
