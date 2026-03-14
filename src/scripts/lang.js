6fda087cd697d11475b57faa83834cefdccdf367
Sophia Blair <sophb.ccjt@proton.me>
2026-03-14 02:11:45 -0300

    fix lang.js

--- src/scripts/lang.js
+++ src/scripts/lang.js
@@ -1,7 +1,7 @@
 const locales = Object.fromEntries(globalThis.localesArray.map(v => [v.lang, v]));
 const getLocaleStr = (key, fallback) => locale[key] ?? fallback;
 const getLocaleTempStr = (key, fallback, vars = {}) => {
-	let str = t(key, fallback);
+	let str = getLocaleStr(key, fallback);
 	Object.entries(vars).forEach(([name, value]) => {
 		str = str.replaceAll(`{${name}}`, value);
 	});
