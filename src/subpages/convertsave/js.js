5d5be8d1ebc4495257ee0a9b5aec0c6c6edc2e55
Sophia Blair <sophb.ccjt@proton.me>
2026-03-29 00:52:38 -0300

    update convertsave and add title heading to translate

--- src/subpages/convertsave/js.js
+++ src/subpages/convertsave/js.js
@@ -1,41 +1,81 @@
-function downloadFileWithContent(filename = `Untitled file - ${Date.now()}`, content = '') {
-	let blob = new Blob([content], { type: 'text/plain' });
-	let url = URL.createObjectURL(blob);
+/*
+== VSCode utils ==
+when adding a new conversion,
+replace all text matching the regex:
+(\[`(NSF\d+\.\d+)\$\{to\}(NSF\d+\.\d+)`\]:[\t ]*)$
+with this:
+$1 // convert $2 to $3
+*/
 
-	let a = document.createElement('a');
-	a.href = url;
-	a.download = filename;
+// /*
+function downloadFileWithContent(filename = `Untitled file [${Date.now()}]`, content = '') {
+    const blob = new Blob([content], { type: 'text/plain' });
+    const url = URL.createObjectURL(blob);
 
-	document.body.appendChild(a);
-	a.click();
-	document.body.removeChild(a);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = filename;
 
-	return content;
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
+
+    return content;
 }
-window.onkeyup = (event) => {
-	const key = event.key.toLowerCase();
-	console.log(key);
-	if (key === 'escape' && document.activeElement === document.body) {
-		window.close();
-	}
-};
+// */
 
-let saveFile = {};
-let saveFileName = '';
-const safeFormats = ['NSF2.0', 'NSF2.1'];
+// elements
 const fileInput = document.getElementById('file');
 const convertSection = document.getElementById('convert');
 const statusElement = document.getElementById('status');
 const convertToElement = document.getElementById('convert-to');
 const convertButton = document.getElementById('convert-btn');
+
+// utils
+let statusTimeouts = [];
+function setStatus(statusText, clearAfterMs = 3e3) {
+	function clearTimeouts() {
+		statusTimeouts.forEach((timeoutId, index)=>{
+			clearTimeout(timeoutId);
+			statusTimeouts[index] = null;
+		});
+		statusTimeouts = statusTimeouts.filter(t=>t);
+	}
+	// clear status
+	if (isNaN(clearAfterMs)) return;
+
+	// clear status
+	clearTimeouts();
+	statusElement.style.display = 'none';
+	if (clearAfterMs === -Infinity) return;
+
+	// set status
+	statusElement.textContent = statusText;
+	statusElement.style.display = 'revert';
+	if (clearAfterMs === Infinity) return; // don't clear status
+	
+	// hide status
+	const timeout = setTimeout(() => {
+		clearTimeouts(); // just in case
+		statusElement.style.display = 'none';
+	}, clearAfterMs);
+	statusTimeouts.push(timeout);
+}
+
+// save file metadata
+let saveFile = {};
+let saveFileName = '';
+const safeFormats = ['NSF2.0', 'NSF2.1'];
+
+// read file
 fileInput.addEventListener('change', (event) => {
 	convertSection.style.display = 'none';
-	statusElement.style.display = 'revert';
 	const file = event.target.files[0];
 
-	if (file) {
+	if (file != null) {
 		saveFileName = file.name;
 
+		setStatus('Initializing file reader...', Infinity);
 		statusElement.textContent = "Initializing file reader...";
 		const reader = new FileReader();
 		reader.onload = function (e) {
@@ -43,123 +83,105 @@ fileInput.addEventListener('change', (event) => {
 			try {
 				content = JSON.parse(content);
 			} catch {
-				statusElement.textContent = "Invalid file format.";
+				setStatus('Invalid file format.', 5e3);
 				return;
 			}
 
-			const convertableFormats = ['NSF1.0', 'NSF2.0'];
-			if (convertableFormats.includes(content?.format)) {
-				statusElement.textContent = "File loaded successfully!";
-				convertSection.style.display = 'revert';
+			// check file type (safe, convertible or invalid)
+			const convertibleFormats = ['NSF1.0', 'NSF2.0'];
+			if (convertibleFormats.includes(content?.format)) {
 				saveFile = content;
+				convertSection.style.display = 'revert';
+				populateFormatDropdown();
+				setStatus('File loaded successfully!', 5e3);
+			} else if (safeFormats.includes(content?.format)) {
+				setStatus('That save file is already safe!', 5e3);
 			} else {
-				statusElement.textContent = "Unsupported, invalid or absent save file format! Please use a valid preferences file.";
+				setStatus('Invalid save file! Please use a valid save file.', 5e3);
 			}
-			setTimeout(() => {
-				statusElement.style.display = 'none';
-			}, 5e3);
 		};
 
-		statusElement.textContent = "Reading file...";
+		setStatus('Reading file...', Infinity);
 		reader.readAsText(file);
 	} else {
-		statusElement.textContent = "File not found.";
-		setTimeout(() => {
-			statusElement.style.display = 'none';
-		}, 3e3);
+		setStatus('File not found.');
 	}
 });
-convertToElement.innerHTML = '';
-safeFormats.forEach(format => {
-	let option = document.createElement('option');
-	option.value = format;
-	option.textContent = format;
-	convertToElement.appendChild(option);
-});
+function populateFormatDropdown() {
+	convertToElement.innerHTML = '';
+	safeFormats.forEach(format => {
+		if (format === saveFile.format) return;
+		let option = document.createElement('option');
+		option.value = format;
+		option.textContent = format;
+		convertToElement.appendChild(option);
+	});
+}
+populateFormatDropdown();
 
-convertButton.onclick = () => {
+const convertButtonCb = convertButton.onclick = () => {
+	setStatus('Starting conversion...', Infinity);
 	const targetFormat = convertToElement.value;
 	if (saveFile.format === targetFormat) {
-		statusElement.textContent = "Cannot convert to own format!";
+		setStatus('Cannot convert to own format!', 5e3);
 		return;
 	}
 
-	statusElement.style.display = 'revert';
-	statusElement.textContent = "Starting conversion...";
-	setTimeout(() => {
-		if (targetFormat === 'NSF2.0') {
-			if (saveFile.format === 'NSF1.0') {
-				let saveFileCopy = structuredClone(saveFile);
-				saveFile = {};
-				for (const [key, value] of Object.entries(saveFileCopy)) {
-					statusElement.textContent = `Converting "${key}"...`;
-					if (key === 'defaultScripts') {
-						continue;
-					}
-					saveFile[key] = value;
-				}
-				statusElement.textContent = 'Finishing up...';
-				saveFile.format = "NSF2.0";
-				downloadFileWithContent(
-					`${saveFileName.replaceAll('.nsf', '')} (Converted).nsf`,
-					JSON.stringify(saveFile)
-				);
+	// convert file
+	const to = '~'; // conversion symbol
+	// conversion operation list
+	const conversions = {
+		[`NSF1.0${to}NSF2.0`]:
+		()=>{
+			delete saveFile.defaultScripts;
+			saveFile.lastVersion = null;
+			saveFile.format = "NSF2.0";
+		},
 
-				statusElement.textContent = 'Done!';
-				convertButton.textContent = 'Close window';
-				convertButton.onclick = () => window.close();
-				setTimeout(() => {
-					statusElement.style.display = 'none';
-				}, 6e3);
-			} else {
-				statusElement.textContent = 'Unsupported!';
-			}
+		[`NSF1.0${to}NSF2.1`]:
+		()=>{
+			delete saveFile.defaultScripts;
+			saveFile.lastChangelogHash = '';
+			saveFile.format = "NSF2.1";
+		},
+
+		[`NSF2.0${to}NSF2.1`]:
+		()=>{
+			delete saveFile.lastVersion;
+			saveFile.lastChangelogHash = '';
+			saveFile.format = "NSF2.1";
 		}
+	}
 
-		if (targetFormat === 'NSF2.1') {
-			if (saveFile.format === 'NSF1.0') {
-				let saveFileCopy = structuredClone(saveFile);
-				saveFile = {};
-				for (const [key, value] of Object.entries(saveFileCopy)) {
-					statusElement.textContent = `Converting "${key}"...`;
-					if (key === 'defaultScripts') {
-						continue;
-					}
-					saveFile[key] = value;
-				}
-				statusElement.textContent = 'Finishing up...';
-				saveFile.format = "NSF2.0";
-				downloadFileWithContent(
-					`${saveFileName.replaceAll('.nsf', '')} (Converted).nsf`,
-					JSON.stringify(saveFile)
-				);
-
-				statusElement.textContent = 'Done!';
-				convertButton.textContent = 'Close window';
-				convertButton.onclick = () => window.close();
-				setTimeout(() => {
-					statusElement.style.display = 'none';
-				}, 6e3);
-			} else if (saveFile.format === 'NSF2.0') {
-				statusElement.textContent = 'Converting...';
-
-				delete saveFile.lastVersion;
-				delete saveFile.lastChangelogHash;
-
-				statusElement.textContent = 'Finishing up...';
-				saveFile.format = "NSF2.1";
-				downloadFileWithContent(
-					`${saveFileName.replace(/\.nsf$/g, '')} (Converted).nsf`,
-					JSON.stringify(saveFile)
-				);
-
-				statusElement.textContent = 'Done!';
-				convertButton.textContent = 'Close window';
-				convertButton.onclick = () => window.close();
-				setTimeout(() => {
-					statusElement.style.display = 'none';
-				}, 6e3);
-			}
+	const convert = conversions?.[`${saveFile.format}${to}${targetFormat}`]
+	if (convert != null) {
+		setStatus('Converting...', Infinity);
+		try {
+			convert();
+			// finalize
+			saveFile.exportDate = Date.now();
+			downloadFileWithContent(
+				`${saveFileName.replace(/\.nsf$/g, '')} (Converted to ${targetFormat}).nsf`,
+				JSON.stringify(saveFile)
+			);
+			setStatus('Done!', 6e3);
+			convertButton.textContent = 'Convert another';
+			convertButton.onclick = () => {
+				fileInput.click()
+				convertButton.onclick = convertButtonCb;
+			};
+		} catch (err) {
+			setStatus(`
+There was an error while converting.
+Error: ${
+	err?.message
+	? `[${err.type}] ${err.message}`
+	: `[RawThrow] ${JSON.stringify(err)}`
+}
+Please try again.
+`.trim())
 		}
-	}, 1.5e3);
+	} else {
+		setStatus(`The conversion from ${saveFile.format} to ${targetFormat} is not supported.`, 5e3)
+	}
 };
