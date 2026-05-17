604ac85c7f49f13f3039df895476780f27048a86
Sophia Blair <148160919+sophb-ccjt@users.noreply.github.com>
2026-05-17 13:57:10 -0300

    letme try something

--- src/scripts/preloader.js
+++ src/scripts/preloader.js
@@ -50,7 +50,7 @@ async function loadScripts(cb = ()=>{}, cacheBusting = true) {
 			src: './scripts/modules/controller_input.js'
 		},
 		{
-			src: './scripts/modules/protoplus.js'
+			src: 'https://raw.githubusercontent.com/sophb-ccjt/protoplus/refs/heads/main/protoplus.js'
 		},
 		{
 			src: './scripts/modules/bgFormat.js'
@@ -66,9 +66,6 @@ async function loadScripts(cb = ()=>{}, cacheBusting = true) {
 		},
 		{
 			src: './scripts/modules/jszip.min.js'
-		},
-		{
-			src: "https://rawcdn.githack.com/sophb-ccjt/protoplus/refs/heads/main/protoplus.js"
 		},
 		// scripts
 		{
