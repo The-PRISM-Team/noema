ceb854427d9ca0e5f1518a4eb8cffb696f7be405
Sophia Blair <sophb.ccjt@proton.me>
2026-08-08 23:53:08 -0300

    test linting loop by simply logging every file's content

--- scripts/lint.js
+++ scripts/lint.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 /* TBD
 const fs = require("fs");
 const synchronizedPrettier = require("@prettier/sync");
@@ -10,9 +12,26 @@ const prettierRules = JSON.parse(
 		"utf8",
 	),
 );
-const webPretty = synchronizedPrettier.format("// code", {
+const prettyFile = synchronizedPrettier.format("// code", {
 	parser: "babel",
 	...prettierRules,
 });
 */
-console.log("Linter TBD");
+const fs = require("fs");
+const path = require("path");
+
+function lintFile(path) {
+	const content = fs.readFileSync(path, { encoding: "utf-8" });
+	console.log(content);
+}
+
+function lintFiles() {
+	const filepaths = fs
+		.readdirSync(".", { recursive: true, withFileTypes: true })
+		.filter((dirent) => dirent.isFile())
+		.map((dirent) => path.join(dirent.parentPath, dirent.name));
+	for (const path of filepaths) {
+		lintFile(path);
+	}
+}
+lintFiles();
